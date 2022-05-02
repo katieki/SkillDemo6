691b56d84f36441e85520d18fbd3e92fcfc2818a
Katie Kim <kak010@ucsd.edu>
2022-05-01 20:41:54 -0700

    Update test that passes

--- SkillDemoTest6.java
+++ SkillDemoTest6.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest6 {
     @Test
     public void substraction() {
-        assertEquals(1, SkillDemo6.subtract(3, 1)); //test that fails
+        assertEquals(1, SkillDemo6.subtract(2, 1)); //test that passes
     }
 }
